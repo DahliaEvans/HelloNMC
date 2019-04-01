0685ab3fead68bc3edd0c684358287d4e8b4e3ba
Dahlia Evans <evans285@ad.nmc.edu>
2019-04-01 14:03:38 -0400

    Add project files.

---
+++ PushingToGitHub/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushingToGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Hello NMC!");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
